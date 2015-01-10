e25ab5b4a9cbbc1c073ec43382ee6aedd4e000ca
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2015-01-10 01:04:19 +0100

    set version to 2.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: InternalsVisibleTo("idseefeld.de.UmbracoAzure.Tests")]
